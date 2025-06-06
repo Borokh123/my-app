11f3de9ffba7600dcf027ffd4ed2296ca9d44b60
Borokh123 <7258999@gmail.com>
2025-06-06 14:38:31 +0300

    06062025Redux-Form + TypeScript + Generic или как решать проблемы

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { ComponentType } from 'react'
 import styles from './FormsControls.module.css'
 
-export const FormControl = ({ input, meta, ...restProps }) => {
-  const hasError = meta.touched && meta.error
-  return (
-    <div className={styles.formControl + " " + (hasError ?  styles.error:'') }>
-      
-        <restProps.child {...restProps} {...input} {...meta} />
 
+export const FormControl = ({
+  input,
+  meta,
+  component: Component,
+  ...restProps
+}=> {
+  const hasError = meta.touched && meta.error
+
+  return (
+    <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
+      <Component {...input} {...restProps} />
       {hasError && <span>{meta.error}</span>}
     </div>
-  
   )
 }
 
